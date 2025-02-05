59b3958699cf398fa5aa880072b447b086e9b565
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-02-05 11:17:40 -0500

    [MIG] account_credit_control_dunning_fees: Migration to version 17.0

--- account_credit_control_dunning_fees/__manifest__.py
+++ account_credit_control_dunning_fees/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Credit control dunning fees",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "category": "Accounting",
